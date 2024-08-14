ad362b838d74e557ae34fac5c2c8ec4efea45ca5
junuo <3236877923@qq.com>
2024-08-14 23:33:49 +0800

    无边框窗口，初步实现阴影

---
+++ customwidget/framelesswidget.cpp
@@ -0,0 +1,63 @@
+﻿#include "stdafx.h"
+
+#include "framelesswidget.h"
+
+#include <QGraphicsDropShadowEffect>
+#include <QPainter>
+#include <QVBoxLayout>
+#include <QPainterPath>
+
+JunuoFrameLessWidget::JunuoFrameLessWidget(QWidget* parent /*= nullptr*/)
+	: QWidget(parent)
+	, m_padding(DPI(2))
+{
+	setWindowFlags(Qt::FramelessWindowHint | Qt::Window);
+	this->setAttribute(Qt::WA_TranslucentBackground);
+	auto shadowEffect = new QGraphicsDropShadowEffect(this);
+	shadowEffect->setOffset(0, 0);
+	QColor shadowColor = QColor(0x757575);
+	shadowColor.setAlphaF(0.37);
+	shadowEffect->setColor(shadowColor);
+	shadowEffect->setBlurRadius(DPI(32));
+	this->setGraphicsEffect(shadowEffect);
+}
+
+JunuoFrameLessWidget::~JunuoFrameLessWidget()
+{
+
+}
+
+void JunuoFrameLessWidget::setMoveable(bool moveable)
+{
+	m_moveable = moveable;
+}
+
+bool JunuoFrameLessWidget::getMoveable() const
+{
+	return m_moveable;
+}
+
+void JunuoFrameLessWidget::setDragable(bool dragable)
+{
+	m_dragable = dragable;
+}
+
+bool JunuoFrameLessWidget::getDragable() const
+{
+	return m_dragable;
+}
+
+void JunuoFrameLessWidget::setPadding(size_t padding)
+{
+	m_padding = padding;
+}
+
+size_t JunuoFrameLessWidget::getPadding() const
+{
+	return m_padding;
+}
+
+void JunuoFrameLessWidget::paintEvent(QPaintEvent* event)
+{
+	QWidget::paintEvent(event);
+}
---
+++ customwidget/framelesswidget.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#ifndef JUNUOUI_EXPORT
+
+#ifdef _JUNUOUI_MODULE_
+#define JUNUOUI_EXPORT __declspec(dllexport)
+#else
+#define JUNUOUI_EXPORT __declspec(dllimport)
+#endif // _JUNUOUI_MODULE_
+
+#endif // JUNUOUI_EXPORT
+
+#include <QWidget>
+
+class JUNUOUI_EXPORT JunuoFrameLessWidget : public QWidget
+{
+	Q_OBJECT
+public:
+	JunuoFrameLessWidget(QWidget* parent = nullptr);
+	~JunuoFrameLessWidget();
+	void setMoveable(bool moveable);
+	bool getMoveable() const;
+	void setDragable(bool dragable);
+	bool getDragable() const;
+	void setPadding(size_t padding);
+	size_t getPadding() const;
+
+protected:
+	void paintEvent(QPaintEvent* event) override;
+
+private:
+	bool m_moveable = true;
+	bool m_dragable = true;
+	size_t m_padding = 0;
+};
